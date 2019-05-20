c0e522afc3b66082daba50034064c098eca106a6
M.A.Soheilian <mha.soheil@gmail.com>
2019-05-20 21:25:31 +0430

    Sign in _ Initial

---
+++ WPF(LMS)/WPF(LMS)/FrontEnd/View/UCSign.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_LMS_.FrontEnd.View
+{
+    /// <summary>
+    /// Interaction logic for UCSign.xaml
+    /// </summary>
+    public partial class UCSign : UserControl
+    {
+        public UCSign()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void Sign_Click(object sender, RoutedEventArgs e)
+        {
+            if (usernametxt.Text == "ali" && passwordtxt.Password == "1234" && RBManager.IsChecked == true)
+            {
+                MessageBox.Show("شما با موفقیت وارد شدید", "Sign in", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+            }
+            else
+            {
+                MessageBox.Show("ERROR !", "Sign in", MessageBoxButton.OK, MessageBoxImage.Error);
+                usernametxt.Text = "";
+                passwordtxt.Password = "";
+
+            }
+        }
+    }
+}
